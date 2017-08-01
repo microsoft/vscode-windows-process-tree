16fe8a9288c72aa1c21c62404304b0144b44e2ea
Daniel Imms <tyriar@tyriar.com>
2017-08-01 00:21:33 -0700

    Tidy up js

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 var native = require('bindings')('windows_process_tree');
 
 function buildProcessTree(processList, rootPid) {
-  
-  // Find the root PID's index in the array
   let rootIndex = processList.findIndex(v => v.pid === rootPid);
   let rootProcess = processList[rootIndex];
+  let childIndexes = processList.filter(v => v.ppid === rootPid);
 
   // Initialize return object
-  var result = {};
-  result.pid = rootProcess.pid;
-  result.name = rootProcess.name;
-  
-  // Construct all child trees recursively
-  let childIndexes = processList.filter(v => v.ppid === rootPid);
-  result.children = childIndexes.map(c => buildProcessTree(processList, c.pid));
-  
-  return result;
+  return {
+    pid: rootProcess.pid,
+    name: rootProcess.name,
+    children: childIndexes.map(c => buildProcessTree(processList, c.pid))
+  };
 }
 
 function getProcessTree(rootPid) {
