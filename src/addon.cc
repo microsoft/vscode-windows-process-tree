e9c8755af629507e066d2f51a3e6cdf54c0b3247
Daniel Imms <tyriar@tyriar.com>
2017-08-03 20:45:17 -0700

    Don't use EnumProcesses
    Rely on CreateToolhelp32Snapshot for list of pids
    
    Fixes #2

--- src/addon.cc
+++ src/addon.cc
@@ -7,51 +7,11 @@
 struct ProcessInfo {
   TCHAR name[MAX_PATH];
   DWORD pid;
+  DWORD ppid;
 };
 
 void GetProcessList(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  DWORD process_ids[1024];
-  DWORD process_ids_size;
   ProcessInfo process_info[1024];
-
-  if (!EnumProcesses(process_ids, sizeof(process_ids), &process_ids_size)) {
-    info.GetReturnValue().SetUndefined();
-    return;
-  }
-
-  DWORD process_ids_count = process_ids_size / sizeof(DWORD);
-
-  // Iterate over each process ID, fetching the process name of each
-  unsigned int process_count = 0;
-  for (unsigned int i = 0; i < process_ids_count; i++) {
-    if (process_ids[i] != 0) {
-      // Get a handle to the process.
-      HANDLE process_handle = OpenProcess(
-          PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, process_ids[i]);
-
-      if (process_handle != NULL) {
-        HMODULE modules;
-        DWORD modules_size;
-
-        // Get the process name and store it and the pid for later
-        if (EnumProcessModules(process_handle, &modules, sizeof(modules),
-                               &modules_size)) {
-          TCHAR process_name[MAX_PATH] = TEXT("<unknown>");
-          GetModuleBaseName(process_handle, modules, process_name,
-                            sizeof(process_name) / sizeof(TCHAR));
-
-          // TODO: Use a parallel array to store name?
-          process_info[process_count] = ProcessInfo();
-          process_info[process_count].pid = process_ids[i];
-          strcpy(process_info[process_count].name, process_name);
-          process_count++;
-        }
-      }
-
-      // Release the handle to the process.
-      CloseHandle(process_handle);
-    }
-  }
 
   // Fetch the parent IDs and store in a map for fast lookup, because
   // CreateToolhelp32Snapshot is a costly operation we only want to do it once.
@@ -60,10 +20,35 @@ void GetProcessList(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   DWORD parent_pid = 0;
   HANDLE snapshot_handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
   process_entry.dwSize = sizeof(PROCESSENTRY32);
+  uint32_t process_count = 0;
   if (Process32First(snapshot_handle, &process_entry)) {
     do {
-      pid_to_ppid_map[process_entry.th32ProcessID] =
-          process_entry.th32ParentProcessID;
+      if (process_entry.th32ProcessID != 0) {
+        // Get a handle to the process.
+        HANDLE process_handle = OpenProcess(
+            PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, process_entry.th32ProcessID);
+
+        if (process_handle != NULL) {
+          HMODULE modules;
+          DWORD modules_size;
+
+          // Get the process name and store it and the pid for later
+          if (EnumProcessModules(process_handle, &modules, sizeof(modules),
+                                &modules_size)) {
+            TCHAR process_name[MAX_PATH] = TEXT("<unknown>");
+            GetModuleBaseName(process_handle, modules, process_name,
+                              sizeof(process_name) / sizeof(TCHAR));
+
+            process_info[process_count].pid = process_entry.th32ProcessID;
+            process_info[process_count].ppid = process_entry.th32ParentProcessID;
+            strcpy(process_info[process_count].name, process_name);
+            process_count++;
+          }
+        }
+
+        // Release the handle to the process.
+        CloseHandle(process_handle);
+      }
     } while (Process32Next(snapshot_handle, &process_entry));
   }
   CloseHandle(snapshot_handle);
@@ -77,7 +62,7 @@ void GetProcessList(const Nan::FunctionCallbackInfo<v8::Value>& info) {
     Nan::Set(object, Nan::New<v8::String>("pid").ToLocalChecked(),
              Nan::New<v8::Number>(process_info[i].pid));
     Nan::Set(object, Nan::New<v8::String>("ppid").ToLocalChecked(),
-        Nan::New<v8::Number>(pid_to_ppid_map[process_info[i].pid]));
+             Nan::New<v8::Number>(process_info[i].ppid));
 
     Nan::Set(result, i, Nan::New<v8::Value>(object));
   }
