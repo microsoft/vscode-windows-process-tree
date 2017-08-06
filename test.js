eb90cc72cd0fea81fe514cf1ca782105913863b3
Daniel Imms <tyriar@tyriar.com>
2017-08-05 22:33:39 -0700

    Add license to code

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-var assert = require('assert');
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+ var assert = require('assert');
 var child_process = require('child_process');
 var native = require('bindings')('windows_process_tree');
 var getProcessTree = require('.');
