8abc1e98cbcb5210e1700ce7c7d327feca8f4879
Daniel Imms <tyriar@tyriar.com>
2017-08-05 22:29:46 -0700

    Make tests less flaky

--- test.js
+++ test.js
@@ -3,6 +3,17 @@ var child_process = require('child_process');
 var native = require('bindings')('windows_process_tree');
 var getProcessTree = require('.');
 
+function pollUntil(condition, cb, interval, timeout) {
+  const retries = 0;
+  if (condition()) {
+    cb();
+  } else {
+    setTimeout(() => {
+      pollUntil(condition, cb, interval, timeout - interval);
+    }, interval);
+  }
+}
+
 describe('getProcessList', () => {
   it('should return a list containing this process', () => {
     var list = native.getProcessList();
@@ -29,28 +40,26 @@ describe('getProcessTree', () => {
     assert.equal(tree.pid, process.pid);
     assert.equal(tree.children.length, 0);
   });
-  
+
   it('should return a tree containing this process\'s child processes', done => {
     cps.push(child_process.spawn('cmd.exe'));
-    setTimeout(() => {
-      var tree = getProcessTree(process.pid);
-      assert.equal(tree.children.length, 1);
-      done();
-    }, 20);
+    pollUntil(() => {
+      return getProcessTree(process.pid).children.length === 1
+    }, () => done(), 20, 500);
   });
-  
+
   it('should return a tree containing this process\'s child processes', done => {
     cps.push(child_process.spawn('powershell.exe'));
     cps.push(child_process.spawn('cmd.exe', ['/C', 'powershell.exe']));
-    setTimeout(() => {
+    pollUntil(() => {
       var tree = getProcessTree(process.pid);
-      assert.equal(tree.children.length, 2);
-      assert.equal(tree.children[0].name, 'powershell.exe');
-      assert.equal(tree.children[0].children.length, 0);
-      assert.equal(tree.children[1].name, 'cmd.exe');
-      assert.equal(tree.children[1].children.length, 1);
-      assert.equal(tree.children[1].children[0].name, 'powershell.exe');
-      done();
-    }, 40);
+      return tree.children.length === 2 &&
+          tree.children[0].name === 'powershell.exe' &&
+          tree.children[0].children.length === 0 &&
+          tree.children[1].name === 'cmd.exe' &&
+          tree.children[1].children &&
+          tree.children[1].children.length === 1 &&
+          tree.children[1].children[0].name === 'powershell.exe';
+    }, () => done(), 20, 500);
   });
 });
