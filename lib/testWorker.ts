767e74edc9942a36da4b0d64a7e4faeb19352d9e
Raymond Zhao <raymondzhao@microsoft.com>
2021-04-08 12:27:05 -0700

    Make module context-aware (#30)
    Fixes microsoft/vscode#120570

---
+++ lib/testWorker.ts
@@ -0,0 +1,21 @@
+import * as assert from 'assert';
+import { isMainThread, parentPort } from 'worker_threads';
+import { getProcessList } from './index';
+
+if (!isMainThread) {
+  new Promise(resolve => {
+    getProcessList(process.pid, (list) => {
+      assert.strictEqual(list.length >= 1, true);
+      assert.strictEqual(list[0].name, 'node.exe');
+      assert.strictEqual(list[0].pid, process.pid);
+      assert.strictEqual(list[0].memory, undefined);
+      assert.strictEqual(list[0].commandLine, undefined);
+      resolve(true);
+    });
+  }).then((res) => {
+    assert.strictEqual(res, true);
+    parentPort.postMessage('done');
+  }, () => {
+    parentPort.postMessage('fail');
+  });
+}
